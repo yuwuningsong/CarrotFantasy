d4c2264d9c5117dbfed1a8858e5ef1705a4e56a5
yuwuningsong <68041603+yuwuningsong@users.noreply.github.com>
2021-03-15 23:11:31 +0800

    上传房屋坍塌部分效果
    房屋health<=0时切换成坍塌模型。（未添加动画、音效、UI）

---
+++ Assets/Scripts/BuildingDeadController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingDeadController : MonoBehaviour
+{
+    [SerializeField] int health = 0;                // 血量
+    [SerializeField] bool isDead = false;           // 是否死亡
+    [SerializeField] GameObject crackedBuilding = null;     // 废墟物体
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IsDead();
+        Dead();
+    }
+
+    // 判断死亡状态
+    void IsDead()
+    {
+        if (health <= 0) isDead = true;
+        // TO DO: UI血条清零
+    }
+
+    // 播放死亡动画
+    void Dead()
+    {
+        if (isDead)
+        {
+            Debug.Log("The building is DEAD!");
+            // TO DO: 播放2D动画效果
+            // TO DO: 播放音效
+            gameObject.SetActive(false);
+            crackedBuilding.SetActive(true);
+        }
+    }
+}
