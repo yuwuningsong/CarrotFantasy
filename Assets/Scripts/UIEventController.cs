933d5060bf78a1e4748f5ad649a909dd7beb1174
yuwuningsong <68041603+yuwuningsong@users.noreply.github.com>
2021-03-18 00:13:24 +0800

    添加失败界面怪物进度条及动画，添加按钮事件
    失败界面怪物进度条传参设置，按钮重玩和选关设置事件（选关未做跳转）

--- Assets/Scripts/UIEventController.cs
+++ Assets/Scripts/UIEventController.cs
@@ -1,9 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIEventController : MonoBehaviour
 {
+    [SerializeField] Button reset = null;           // 重玩本关按钮
+    [SerializeField] Button backToMenu = null;      // 回到主菜单按钮
+
+    private void Awake()
+    {
+        reset.onClick.AddListener(ResetLevel);
+        backToMenu.onClick.AddListener(BackToMenu);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +31,21 @@ public class UIEventController : MonoBehaviour
     void StopGame()
     {
         Time.timeScale = 0;
+    }
+
+    // 重玩本关
+    void ResetLevel()
+    {
+        Debug.Log("Reset Level!");
+        // TO DO: 重新加载本关
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+    }
+
+    // 返回选关界面
+    void BackToMenu()
+    {
+        Debug.Log("Back To Menu!");
+        // TO DO: 跳转至选关界面
     }
 }
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] Text monsterNum = null;    // 清除的怪物波数UI文本
+    [SerializeField] Text monsterNumTotal = null;   // 总共的怪物波数UI文本
+    [SerializeField] Slider slider = null;          // 清除的怪物条
+
+    public int MonsterNum { get { return 3; } set { } }         // 清除的怪物波数
+    public int MonsterNumTotal { get { return 15; } set { } }    // 总共的怪物波数
+
+    private void Awake()
+    {
+        monsterNum.text = MonsterNum.ToString();
+        monsterNumTotal.text = MonsterNumTotal.ToString();
+
+        slider.maxValue = MonsterNumTotal;
+        slider.value = MonsterNum / MonsterNumTotal;
+    }
+}
