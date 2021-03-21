c9b88ae30c7535830c564d9a8fc7f09d498eb450
give-to <1139019079@qq.com>
2021-03-22 01:53:34 +0800

    怪物移动和碰撞器检测

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -7,6 +7,9 @@ public class Monster : MonoBehaviour
     public float speed = 10;
     private Transform[] positions;
     private int index = 0;
+
+    public GameObject explosionEffectPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +23,7 @@ public class Monster : MonoBehaviour
             return;//怪物已经到达目标位置，游戏失败
 
         //两个位置相减得到移动方向,nomalized取得该方向的单位向量
-        transform.Translate((positions[index].position - transform.position).normalized * Time.deltaTime * speed);
+        transform.Translate((-positions[index].position + transform.position).normalized * Time.deltaTime * speed);
         if(Vector3.Distance(positions[index].position, transform.position) < 0.2f)
         {
             index++;
@@ -31,5 +34,16 @@ public class Monster : MonoBehaviour
     void Update()
     {
         monsterAction();
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Building")
+        {
+            col.GetComponent<BuildingHurtController>().ReduceHealth();
+            //GameObject.Instantiate(explosionEffectPrefab, transform.position, transform.rotation);
+            Destroy(this.gameObject);
+
+        }
     }
 }
