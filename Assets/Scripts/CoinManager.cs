b993651f649427512215fc9aae45796acb5a7727
yuwuningsong <68041603+yuwuningsong@users.noreply.github.com>
2021-04-16 13:34:25 +0800

    添加金币系统，怪物死亡增加金币
    金币系统CoinManager单例类，怪物死亡爆炸金币增加金币未测试。

---
+++ Assets/Scripts/CoinManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinManager : MonoBehaviour
+{
+    public static CoinManager coinManager;
+
+    [SerializeField] int coinNum = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinManager = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 增加金币数
+    public void AddCoins(int coin)
+    {
+        coinNum += coin;
+    }
+
+    // 减少金币数，成功返回true，不成功返回false
+    public bool UseCoins(int coin)
+    {
+        if (coinNum < coin) return false;
+        coinNum -= coin;
+        return true;
+    }
+}
---
+++ Assets/Scripts/MonsterCoinController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterCoinController : MonoBehaviour
+{
+    [SerializeField] int coinNum = 0;
+    [SerializeField] GameObject coin = null;
+    [SerializeField] int coinNumVisible = 0;
+
+    private List<GameObject> coins = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 爆炸产生金币
+    void CreateCoins()
+    {
+        for (int i = 0; i < coinNumVisible; i++)
+        {
+            coins.Add(Instantiate(coin, transform));
+        }
+        CoinManager.coinManager.AddCoins(coinNum);
+    }
+
+    // 销毁金币
+    void DestroyCoins()
+    {
+        for (int i = 0; i < coinNumVisible; i++)
+        {
+            Destroy(coins[0]);
+        }
+    }
+}
